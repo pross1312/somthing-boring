75b8a2928be78766beb033cf06e769e20d53f1a5
dvtuong <dangvinhtuong12@gmail.com>
2023-06-29 00:45:32 +0700

    update to look cleaner

--- aoc02/main.rs
+++ aoc02/main.rs
@@ -1,15 +1,8 @@
-use std::fs::File;
-use std::io::prelude::*;
 fn solve() {
-    let mut input_file = File::open("input.txt").expect("Can't open file");
-    let mut input = String::new();
-    input_file.read_to_string(&mut input).expect("Can't read input");
+    let input = std::fs::read_to_string("input.txt").unwrap();
     let mut part1 = 0;
     let mut part2 = 0;
-    for game in input.split('\n') {
-        if game == "" {
-            continue;
-        }
+    for game in input.split_terminator('\n') {
         let a: Vec<&str> = game.split(' ').collect();
         let you = a[1].as_bytes()[0] - b'X';
         let oppo = a[0].as_bytes()[0] - b'A';
@@ -21,7 +14,7 @@ fn solve() {
 }
 // 1 2 3
 // r p s
-fn match_point(mut you: u8, mut oppo: u8) -> i32 {
+fn match_point(you: u8, oppo: u8) -> i32 {
     let outcome = if you == oppo {3} else if (you+1)%3 == oppo {0} else {6};
     (you + 1 + outcome).into()
 }
